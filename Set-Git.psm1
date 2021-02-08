39084a0196e711b1a9cf641bdc25b9ef67ae78e0
Ben Jordan <ben@poecoh.com>
2021-02-08 14:38:57 -0500

    Create Set-Git.psm1

---
+++ Set-Git.psm1
@@ -0,0 +1,19 @@
+Function Get-Git.psm1 {
+    Param (
+        $Org,
+        Repo,
+        $Dir,
+        $Token
+    )
+    $GitHub = "https://api.github.comm/repos/$Org/$Repo/contents/$Dir"
+    $Rest = @{
+        Uri = $GitHub
+        Method = 'Get'
+        Headers = @{'Authorization' = "Basic $Token"}
+        Body = $Null
+    }
+    $Sha = (Invoke-RestMethod @Rest).sha
+    
+    
+    $Content = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($Content))
+}
