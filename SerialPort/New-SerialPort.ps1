4f695e9477bc8e3ac4d9edcfe91968a5a5d886b8
Ben Jordan <ben@poecoh.com>
2021-02-12 22:29:29 -0500

    Working. Need to fill in bauds.
    Signed-off-by: Ben Jordan <ben@poecoh.com>

--- SerialPort/New-SerialPort.ps1
+++ SerialPort/New-SerialPort.ps1
@@ -1,20 +1,28 @@
+$Classes = @(
+    'https://raw.githubusercontent.com/Cohors1316/PowerShell/master/SerialPort/Classes/ValidFriendlyNames.ps1',
+    'https://raw.githubusercontent.com/Cohors1316/PowerShell/master/SerialPort/Classes/ValidPortNames.ps1',
+    'https://raw.githubusercontent.com/Cohors1316/PowerShell/master/SerialPort/Classes/ValidServices.ps1'
+)
+ForEach ($Class In $Classes) {Invoke-RestMethod -Uri $Class -Method Get | Invoke-Expression}
+
 Function New-SerialPort {
     [CmdletBinding()]
     [OutputType([System.IO.Ports.SerialPort])]
 
     Param (
 
-        # PortName
+        # PortName. Uses a dynamic class to show all currenlty valid port names. Only what is currently plugged in will be shown.
         [Parameter()]
         [ValidateSet([ValidPortNames])]
-        $PortName,
+        [System.String]$PortName,
 
-        # BaudRate
+        # BaudRate, I'll add the others when I know everything works
         [Parameter()]
         [ValidateSet(
-            9600
+            9600,
+            115200
         )]
-        $BaudRate = 9600,
+        [System.Int32]$BaudRate = 9600,
 
         # Parity
         [Parameter()]
@@ -25,12 +33,12 @@ Function New-SerialPort {
             [System.IO.Ports.Parity]::Mark,
             [System.IO.Ports.Parity]::Space
         )]
-        $Parity = [System.IO.Ports.Parity]::None,
+        [System.IO.Ports.Parity]$Parity = [System.IO.Ports.Parity]::None,
 
         # DataBits
         [Parameter()]
         [ValidateSet(5,6,7,8)]
-        $DataBits = 8,
+        [System.Int32]$DataBits = 8,
 
         # StopBits
         [Parameter()]
@@ -40,53 +48,81 @@ Function New-SerialPort {
             [System.IO.Ports.StopBits]::OnePointFive,
             [System.IO.Ports.StopBits]::Two
         )]
-        $StopBits = [System.IO.Ports.StopBits]::One,
+        [System.IO.Ports.StopBits]$StopBits = [System.IO.Ports.StopBits]::One,
 
-        # NewLine
+        # NewLine `r = CR, `n = LF/NL, `r`n = CRLF
         [Parameter()]
-        [ValidateSet("`r","`n","`r`n")]
-        $NewLine = "`r`n",
+        [System.String]$NewLine = "`r`n",
         
         # DtrEnable
         [Parameter()]
         [Switch]$DtrEnable,
 
+        # RtsEnable
         [Parameter()]
         [Switch]$RtsEnable,
 
-        # PnPDevice.FriendlyName
+        # ReadTimeout in milliseconds. Currently 1 to 100000.
+        [Parameter()]
+        [ValidateRange(1,100000)]
+        [System.Int32]$ReadTimeout = 10000,
+
+        # Filter by FriendlyName. This uses a dynamic class to fetch all currently valid names. If one is not plugged in it will not show.
         [Parameter()]
         [ValidateSet([ValidFriendlyNames])]
-        $FriendlyName,
+        [System.String]$FriendlyName,
 
-        # PnPDevice.Service
+        # Filter by Service. This uses a dynamic class to fetch all currently valid services. If one is not plugged in it will not show.
         [Parameter()]
         [ValidateSet([ValidServices])]
-        $Service
+        [System.String]$Service
 
     )
 
     Process {
 
-        If ($Null -Eq $PortName -And $Null -NE $FriendlyName) {
-            $PortName = (Get-PnPDevice).Where({
-                $_.Class -Eq 'Ports' -And `
-                $_.Present -Eq $True -And `
-                $_.FriendlyName -Match $FriendlyName
-            })
-            $PortName = ([RegEx]'\((COM\d+)\)').Match($FriendlyName).Groups[1].Value
-        }
-        ElseIf ($Null -Eq $PortName -And $Null -Eq $FriendlyName) {
-            Throw('Must supply a PortName or FriendlyName')
+        If ($PortName -Eq '') {
+
+            $Ports = (Get-PnPDevice).Where({$_.Class -Eq 'Ports' -And $_.Present -Eq $True -And $_.Status -Eq 'OK'})
+            If ($Service -NE '' -And $Ports.Length -GT 1) {$Ports = $Ports.Where({$_.Service -Eq $Service})}
+            If ($FriendlyName -NE '' -And $Ports.Length -GT 1) {
+                $Ports = $Ports.Where({$_.FriendlyName -Match $FriendlyName})
+            }
+            If ($Ports.Length -GT 1) {Throw 'More than one port has been caught by the selected filters'}
+            $PortName = ([RegEx]'\((COM\d+)\)').Match($Ports.FriendlyName).Groups[1].Value
+
         }
 
-        $Port = [System.IO.Ports.SerialPort]::new($PortName,$BaudRate,$Parity,$DataBits,$StopBits)
-
-        If ($DtrEnable.IsPresent) {$Port.DtrEnable = $True}
-        
-        If ($RtsEnable.IsPresent) {$Port.RtsEnable = $True}
+        $Port = New-Object -TypeName System.IO.Ports.SerialPort -Property @{
+            PortName = $PortName
+            BaudRate = $BaudRate
+            Parity = $Parity
+            DataBits = $DataBits
+            StopBits = $StopBits
+            NewLine = $NewLine
+            ReadTimeout = $ReadTimeout
+            DtrEnable = If ($DtrEnable.IsPresent) {$True} Else {$False}
+            RtsEnable = If ($RtsEnable.IsPresent) {$True} Else {$False}
+        }
 
         Write-Output -InputObject $Port
+
     }
+
+    <#
+    .Synopsis
+    To do
+
+    .Example
+    $Port = New-SerialPort -PortName COM3 -BaudRate 115200 -RtsEnable -DtrEnable
+
+    .Example
+    $Port = New-SerialPort COM3 9600 None 8 One "`r`n"
+
+
+    .Example
+    # Unknown PortName or moving target, this will take longer to run as it has to search Get-PnPDevice
+    $Port = New-SerialPort -FriendlyName 'USB Serial Device' -Service usbser
+    #>
 
 }
