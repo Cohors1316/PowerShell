e3b5be6b011afdac79af851319e5ddbd61608289
Ben Jordan <ben@poecoh.com>
2021-02-15 08:29:34 -0500

    Update ValidDynamicFriendlyNames.ps1

--- Classes/SerialPort/ValidDynamicFriendlyNames.ps1
+++ Classes/SerialPort/ValidDynamicFriendlyNames.ps1
@@ -1,6 +1,6 @@
 Class ValidDynamicFriendlyNames : System.Management.Automation.IValidateSetValuesGenerator {
     [System.String[]]GetValidValues() {
-        $FriendlyNames =  (Get-PnPDevice).Where({$_.Class -Eq 'Ports'}).FriendlyName
+        $FriendlyNames =  (Get-PnPDevice -Class Ports).FriendlyName
         $Array = [System.Collections.Generic.List[String]]::New()
         ForEach ($FriendlyName In $FriendlyNames) {
             $Array.Add(([RegEx]'(.*)\(COM\d+\)').Match($FriendlyName).Groups[1].Value.TrimEnd(' '))
