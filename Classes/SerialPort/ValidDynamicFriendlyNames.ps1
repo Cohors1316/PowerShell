9a7cebf0230869eb83c967fc30ccf977b561f1eb
Ben Jordan <ben@poecoh.com>
2021-02-15 10:57:40 -0500

    Need for Speed

--- Classes/SerialPort/ValidDynamicFriendlyNames.ps1
+++ Classes/SerialPort/ValidDynamicFriendlyNames.ps1
@@ -1,6 +1,6 @@
 Class ValidDynamicFriendlyNames : System.Management.Automation.IValidateSetValuesGenerator {
     [System.String[]]GetValidValues() {
-        $FriendlyNames =  (Get-PnPDevice -Class Ports -Status OK -PresentOnly).FriendlyName
+        $FriendlyNames =  (Get-CimInstance -ClassName Win32_PnPEntity -Filter "PnPClass = 'Ports'").Name
         $Array = [System.Collections.Generic.List[String]]::New()
         ForEach ($FriendlyName In $FriendlyNames) {
             $Array.Add(([RegEx]'(.*)\(COM\d+\)').Match($FriendlyName).Groups[1].Value.TrimEnd(' '))
