3d14c957a0ca5717bf7e9c30b51ddd19d3d17731
Ben Jordan <ben@poecoh.com>
2021-02-18 09:16:53 -0500

    experiment
    Signed-off-by: Ben Jordan <ben@poecoh.com>

--- Classes/GitHub/ValidDynamicClasses.ps1
+++ Classes/GitHub/ValidDynamicClasses.ps1
@@ -1,6 +1,6 @@
 Class ValidDynamicClasses : System.Management.Automation.IValidateSetValuesGenerator {
     [System.String[]]GetValidValues() {
-        $Folders = (Invoke-RestMethod -Uri https://api.github.com/Cohors1316/PowerShell/contents/Classes/ -Method Get).path
+        $Folders = (Invoke-RestMethod -Uri "$GitApi/contents/Classes/" -Method Get).path
         $Classes = [System.Collections.Generic.List[System.Object]]
         ForEach ($Folder In $Folders) {
             $Classes.Add($(Invoke-RestMethod -Uri "api.powershell.poecoh.com/contents/$Folder/"))
