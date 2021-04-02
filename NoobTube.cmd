53bd38efdc09372d8146cd1eca022435b36ec263
Ben Jordan <ben@poecoh.com>
2021-04-02 15:59:20 -0400

    Update NoobTube.cmd

--- NoobTube.cmd
+++ NoobTube.cmd
@@ -1,4 +1,4 @@
-REM This will execute the first ps1 file located in the same directory,
+REM This will execute the first ps1 file located in the same directory as this file,
 REM usefull for letting people execute ps1s without configuring anything.
 REM So named because while it can be extremely useful, it can also be extremely dangerous.
 REM I thought about making it execute every ps1, but that could get very dangerous very fast.
