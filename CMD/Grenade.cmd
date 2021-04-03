6ab9f12b7500d64d32fe36e62282012c4a7961cf
Ben Jordan <ben@poecoh.com>
2021-04-03 13:41:07 -0400

    Update Grenade.cmd

--- CMD/Grenade.cmd
+++ CMD/Grenade.cmd
@@ -5,6 +5,6 @@ REM usefull for letting people execute ps1s without configuring anything.
 REM So named because while it can be extremely useful, it can also be extremely dangerous.
 REM I thought about making it execute every ps1, but that could get very dangerous very fast.
 
-For /f %%i in ('Dir /b *.ps1') Do Set VAR=%%i
+For /f %%i In ('Dir /b *.ps1') Do Set Var=%%i
 
-PowerShell -ExecutionPolicy Bypass -NoProfile -File %VAR%
+PowerShell -ExecutionPolicy Bypass -NoProfile -File %Var%
