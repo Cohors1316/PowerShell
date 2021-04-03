959319b145386fc700641baafff14a4b74c52083
Ben Jordan <ben@poecoh.com>
2021-04-03 15:16:19 -0400

    Update Nuke.cmd

--- CMD/Nuke.cmd
+++ CMD/Nuke.cmd
@@ -1,7 +1,7 @@
-@ECHO OFF
+@Echo Off
 
-REM This will execute all ps1 files located in the same directory,
-REM usefull for letting people execute ps1s without configuring anything.
-REM Warning, can be extremely dangerous.
+Rem This will execute all ps1 files located in the same directory,
+Rem usefull for letting people execute ps1s without configuring anything.
+Rem Warning, can be extremely dangerous.
 
 For /F "TOKENS=*" %%A In ('Dir /b *.ps1') Do Start "" "PowerShell" -File "%%~A"
