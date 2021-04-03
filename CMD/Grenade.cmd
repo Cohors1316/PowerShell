a689197cdbf14d707606251a622da0754c1285bb
Ben Jordan <ben@poecoh.com>
2021-04-03 13:39:03 -0400

    Update Grenade.cmd

--- CMD/Grenade.cmd
+++ CMD/Grenade.cmd
@@ -1,7 +1,10 @@
+@ECHO OFF
+
 REM This will execute the first ps1 file located in the same directory as this file,
 REM usefull for letting people execute ps1s without configuring anything.
 REM So named because while it can be extremely useful, it can also be extremely dangerous.
 REM I thought about making it execute every ps1, but that could get very dangerous very fast.
 
-@ECHO OFF
-PowerShell -ExecutionPolicy Bypass -NoProfile -Command "& {Start-Process PowerShell -NoProfile -ExecutionPolicy Bypass -ArgumentList ""-File '$((Get-ChildItem -Filter *.ps1)[0].FullName)'""}"
+For /f %%i in ('Dir /b *.ps1') Do Set VAR=%%i
+
+PowerShell -ExecutionPolicy Bypass -NoProfile -File %VAR%
