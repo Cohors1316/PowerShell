d5d92e11a3daa909d883c9243a4401dc6653bb3c
Ben Jordan <ben@poecoh.com>
2021-04-03 15:17:09 -0400

    Update Nuke.cmd

--- CMD/Nuke.cmd
+++ CMD/Nuke.cmd
@@ -4,4 +4,4 @@ Rem This will execute all ps1 files located in the same directory,
 Rem usefull for letting people execute ps1s without configuring anything.
 Rem Warning, can be extremely dangerous.
 
-For /F "TOKENS=*" %%A In ('Dir /b *.ps1') Do Start "" "PowerShell" -File "%%~A"
+For /F "TOKENS=*" %%A In ('Dir /b *.ps1') Do Start "" "PowerShell" -ExecutionPolicy Bypass -NoProfile -File "%%~A"
